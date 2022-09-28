0cb5c1b88431f0047f306670b53e7feab39ba448
Kunal Badnagre <101064122+kunalbadnagre@users.noreply.github.com>
2022-09-28 08:57:05 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,21 @@
 
+function calculate(){
+    let bill = document.getElementById('bill').value;
+    let percent = document.getElementById('tipPercent').value;
+    let person = document.querySelector('#person').value;
+    if(person == ""){
+        person = "1";
+    }
+
+    let tipAmount = ((bill/100)*percent)/person;
+    let roundOffTip = tipAmount.toFixed(2);
+    document.getElementById('tipAmount').innerHTML = "$"+roundOffTip;
+    
+    let totalAmount = (bill/person)+tipAmount;
+    let roundOffTotal = totalAmount.toFixed(2);
+    document.getElementById('totalAmount').innerHTML = "$"+roundOffTotal;
+
+    document.getElementById('bill').value = "";
+    document.getElementById('tipPercent').value = "";
+    document.getElementById('person').value = "";
+}
